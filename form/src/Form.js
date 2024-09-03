a818fbab15cad89879a68fca9ba391dcf7473d11
Ashutosh Kumar <ashutoshkumarssm352@gmail.com>
2024-09-04 00:16:42 +0530

    commit initial

---
+++ form/src/Form.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Form.css'
+
+function Form() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:5000/api/form', { name, email, message });
+            alert('Data saved successfully');
+            setName('');
+            setEmail('');
+            setMessage('');
+        } catch (error) {
+            alert('Error saving data');
+        }
+    };
+
+    return (
+        <div className='form'>
+            <h1>Contact Form</h1><br />
+            <form onSubmit={handleSubmit} autoComplete='off'>
+                <div>
+                    <input type="text" id='name' value={name} onChange={(e) => setName(e.target.value)} placeholder=' Enter your fullName' required autoFocus /> <br /><br />
+                </div>
+                <div>
+                    <input type="email" id='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder=' Enter your emailAddress' required /> <br /><br />
+                </div>
+                <div>
+                    <textarea id='msg' value={message}rows={3} onChange={(e) => setMessage(e.target.value)} placeholder=' Type your message....' required /> <br /><br />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Form;
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Connect to MongoDB
+try {
+    mongoose.connect('mongodb://localhost:27017/form')
+    console.log('Database connected')
+} catch (error) {
+    console.log('connection failed')
+}
+
+// Define Schema and Model
+const FormSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    message: String,
+});
+
+const Form = mongoose.model('Form', FormSchema);
+
+// Routes
+app.post('/api/form', async (req, res) => {
+    try {
+        const formData = new Form(req.body);
+        await formData.save();
+        res.status(201).json({ message: 'Data saved successfully' });
+    } catch (error) {
+        res.status(400).json({ message: 'Error saving data', error });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on : ${PORT}`);
+});
